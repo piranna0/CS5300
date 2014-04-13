4f2040b7455f9ad4e80d7edc3799e2bf4b4685af
James Feher <jkf49@cornell.edu>
2014-04-12 23:39:05 -0400

    Added View.java

--- src/myPackage/MyServlet.java
+++ src/myPackage/MyServlet.java
@@ -37,6 +37,8 @@ public class MyServlet extends HttpServlet
 	private final byte SESSIONWRITE = 1; // operation code
 	private final int PORT = 5300;
 	
+	private static View view = new View();
+	
 	// Session State table, aka hash map used to store session information
 	// K: sessionID, V: SessionState
 	private ConcurrentHashMap<Integer, SessionState> map = new ConcurrentHashMap<Integer, SessionState>();
---
+++ src/myPackage/View.java
@@ -0,0 +1,44 @@
+package myPackage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
+
+public class View {
+	private HashSet<String> view;
+	public View(){
+		view = new HashSet<String>();
+	}
+	
+	public static void shrink(View v, int k){
+		int i = 0;
+		ArrayList<String> arr = new ArrayList<String>(v.view);
+		
+		//Creates random permutation, then remove first size - k entries
+		Collections.shuffle(arr); 
+		while(v.view.size() > k){
+			v.view.remove(arr.get(i));
+			i++;
+		}
+	}
+	
+	public static void insert(View v, String svrID){
+		v.view.add(svrID);
+	}
+	
+	public static void remove(View v, String svrID){
+		v.view.remove(svrID);
+	}
+	
+	public static String choose(View v){
+		Object[] arr = v.view.toArray();
+		Random r = new Random();
+		int index = r.nextInt(arr.length);
+		return (String) arr[index];
+	}
+	
+	public static void union(View v, View w){
+		v.view.addAll(w.view);
+	}
+}
