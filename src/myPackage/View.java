fe8745ff53afb72879df81054373e19fe249c97e
James Feher <jkf49@cornell.edu>
2014-04-14 05:09:19 -0400

    Loopback addresses and bad addresses not added to view to db

--- src/myPackage/View.java
+++ src/myPackage/View.java
@@ -9,43 +9,43 @@ import java.util.Random;
 
 public class View {
 	private HashSet<String> view;
-	
-//	public static void main(String[] args){
-//		View v = new View();
-//		System.out.println(choose(v));
-//		insert(v, "1.1.1.1");
-//		insert(v, "1.1.1.2");
-//		insert(v, "1.1.1.3");
-//		insert(v, "1.1.1.4");
-//		System.out.println(choose(v));
-//		
-//		remove(v, "1.1.1.2");
-//		System.out.println(v.view);
-//		
-//		View v2 = new View();
-//		insert(v2, "1.1.1.3");
-//		insert(v2, "1.1.1.4");
-//		insert(v2, "1.1.1.5");
-//		
-//		union(v, v2);
-//		System.out.println(v.view);
-//		System.out.println(v2.view);
-//		
-//		shrink(v,2);
-//		shrink(v2,1);
-//		
-//		System.out.println(v.view);
-//		System.out.println(v2.view);
-//	}
-	
+
+	//	public static void main(String[] args){
+	//		View v = new View();
+	//		System.out.println(choose(v));
+	//		insert(v, "1.1.1.1");
+	//		insert(v, "1.1.1.2");
+	//		insert(v, "1.1.1.3");
+	//		insert(v, "1.1.1.4");
+	//		System.out.println(choose(v));
+	//		
+	//		remove(v, "1.1.1.2");
+	//		System.out.println(v.view);
+	//		
+	//		View v2 = new View();
+	//		insert(v2, "1.1.1.3");
+	//		insert(v2, "1.1.1.4");
+	//		insert(v2, "1.1.1.5");
+	//		
+	//		union(v, v2);
+	//		System.out.println(v.view);
+	//		System.out.println(v2.view);
+	//		
+	//		shrink(v,2);
+	//		shrink(v2,1);
+	//		
+	//		System.out.println(v.view);
+	//		System.out.println(v2.view);
+	//	}
+
 	public View(){
 		view = new HashSet<String>();
 	}
-	
+
 	public static void shrink(View v, int k){
 		int i = 0;
 		ArrayList<String> arr = new ArrayList<String>(v.view);
-		
+
 		//Creates random permutation, then remove first size - k entries
 		Collections.shuffle(arr); 
 		while(v.view.size() > k){
@@ -53,15 +53,23 @@ public class View {
 			i++;
 		}
 	}
-	
+
 	public static void insert(View v, String svrID){
-		v.view.add(svrID);
+		try {
+			InetAddress s = InetAddress.getByName(svrID);
+			if(!s.isLoopbackAddress()){
+				v.view.add(svrID);
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static void remove(View v, String svrID){
 		v.view.remove(svrID);
 	}
-	
+
 	public static String choose(View v){
 		Object[] arr = v.view.toArray();
 		if(arr.length == 0){
@@ -71,21 +79,21 @@ public class View {
 		int index = r.nextInt(arr.length);
 		return (String) arr[index];
 	}
-	
+
 	public static void union(View v, View w){
 		v.view.addAll(w.view);
 	}
-	
+
 	public static View copy(View v){
 		View ret = new View();
 		union(ret, v);
 		return ret;
 	}
-	
+
 	public static HashSet<String> getIPs(View v){
 		return v.view;
 	}
-	
+
 	public String toString(){
 		String str = "";
 		for(String s : view){
