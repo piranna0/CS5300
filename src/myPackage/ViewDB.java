1176c03a1f6c231812ad6b768e2385d81c32a610
James Feher <jkf49@cornell.edu>
2014-04-14 21:57:54 -0400

    Removed comments and print lines from View and ViewDB

--- src/myPackage/ViewDB.java
+++ src/myPackage/ViewDB.java
@@ -29,56 +29,10 @@ public class ViewDB {
 	static String trueValue = "True";
 	static String falseValue = "False";
 
-	public static void main(String[] args){
-		//		
-		//		
-		try {
-			sdb = new AmazonSimpleDBClient(new PropertiesCredentials(
-					ViewDB.class.getResourceAsStream("AwsCredentials.properties")));
-			System.out.println(sdb.listDomains());
-
-			//If sdb doesn't exist somehow
-			if(!sdb.listDomains().getDomainNames().contains(myDomain)){
-				sdb.createDomain(new CreateDomainRequest(myDomain));
-			}
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//		
-		//		
-		////		View v = new View();
-		////		HashSet<String> arr = new HashSet<String>();
-		////		View.insert(v, "1.1.1.1");
-		////		View.insert(v, "1.1.1.2");
-		////		writeSDBView(v);
-		////		try {
-		////			Thread.sleep(3000);
-		////		} catch (InterruptedException e) {
-		////			// TODO Auto-generated catch block
-		////			e.printStackTrace();
-		////		}
-		readSDBView(5);
-		//		try {
-		//			System.out.println(inetaddrToString(InetAddress.getLocalHost()));
-		//			System.out.println(convertToReadableIP(inetaddrToString(InetAddress.getLocalHost())));
-		//			System.out.println(InetAddress.getByAddress(inetaddrToString(InetAddress.getLocalHost()).getBytes()).getHostAddress());
-		//		} catch (UnknownHostException e) {
-		//			// TODO Auto-generated catch block
-		//			e.printStackTrace();
-		//		}
-	}
-
-	private static String inetaddrToString(InetAddress addr) {
-		return new String(addr.getAddress());
-	}
-
 	public static void init(){
 		try {
 			sdb = new AmazonSimpleDBClient(new PropertiesCredentials(
 					ViewDB.class.getResourceAsStream("AwsCredentials.properties")));
-			System.out.println(sdb.listDomains());
 
 			//If sdb doesn't exist somehow
 			if(!sdb.listDomains().getDomainNames().contains(myDomain)){
@@ -103,44 +57,21 @@ public class ViewDB {
 		//Get ip addresses stored in SimpleDB
 		for(Item item : sr.getItems()){
 			for(Attribute attr : item.getAttributes()){
-				if(attr.getName().equals(IPAttribute)){
-					if (!attr.getValue().equals("0:0:0:0:0:0:0:1"))		// TODO i added this
-						View.insert(v, attr.getValue());
-//						System.out.println(InetAddress.getByName(attr.getValue()));
-					//					System.out.println("attr:" + attr.getValue());
-					break;
+				if(attr.getName().equals(IPAttribute)){	
+					View.insert(v, attr.getValue());
 				}
 			}
 		}
 		return v;
 	}
 
-	//For testing readSDBView only
-	//	public static String convertToReadableIP(String addr) {
-	//		byte[] bytes = addr.getBytes();
-	//		InetAddress a;
-	//		try {
-	//			a = InetAddress.getByAddress(bytes);
-	//			return a.getHostAddress();
-	//		} catch (UnknownHostException e) {
-	//			// TODO Auto-generated catch block
-	//			e.printStackTrace();
-	//		}
-	//		return null;
-	//	}
-
-	/*
-	 * TODO: Change Write so that it always writes ViewSz elements and then size. The deleting part scares me a little with concurrent accesses
-	 */
-
-
 	//Takes two parameters:
 	//1: List of ip addresses to be written
 	//2: number of attributes stored in database previously(must remove extras)
 	public static void writeSDBView(View v, int ViewSz){
-		int size = 0;
 		int i=0;
 		HashSet<String> ips = View.getIPs(v);
+		
 		//Replace previous attributes with new IP addresses
 		for(String ip : ips){
 			try {
@@ -167,6 +98,7 @@ public class ViewDB {
 			}
 		}
 
+		//Extra entries filled in with "0.0.0.0" and marked invalid
 		for(; i < ViewSz; i++){
 			ReplaceableAttribute replaceAttributeType = new ReplaceableAttribute().withName(typeAttribute).
 					withValue(serverType).withReplace(true);
@@ -181,22 +113,6 @@ public class ViewDB {
 					.withAttributes(replaceAttributeType, replaceAttribute, replaceAttributeIndex, replaceAttributeValid));
 		}
 
-
-		//Get size of database so we can overwrite old entries
-		//		String selectExpression = "select * from " + myDomain + " where Type = '" + sizeType + "'";
-		//		SelectRequest req = new SelectRequest(selectExpression);
-		//		ArrayList<String> arr = new ArrayList<String>();
-		//		for(Item item : sdb.select(req).getItems()){
-		//			for(Attribute attr : item.getAttributes()){
-		//				if(attr.getName().equals(sizeAttribute)){
-		//					arr.add(attr.getValue());
-		//					size = Integer.parseInt(attr.getValue());
-		//					break;
-		//				}
-		//			}
-		//		}
-		//		
-		//		//Add size item
 		ReplaceableAttribute replaceAttributeSize = new ReplaceableAttribute().withName(sizeAttribute).
 				withValue(Integer.toString(ips.size())).withReplace(true);		
 		ReplaceableAttribute replaceAttribute = new ReplaceableAttribute().withName(typeAttribute).
@@ -204,11 +120,5 @@ public class ViewDB {
 
 		sdb.putAttributes(new PutAttributesRequest().withDomainName(myDomain).withItemName(myItem + i)
 				.withAttributes(replaceAttribute,replaceAttributeSize));
-		//		i++;//Delete all items after size item
-		//		
-		//		//Delete extra attributes
-		//		for(; i < size+1; i++){
-		//			sdb.deleteAttributes(new DeleteAttributesRequest(myDomain, myItem + i));
-		//		}
 	}
 }
